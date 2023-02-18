205aa598d1d66b2e3f7a21efcf37bb69aa48c8b2
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2023-02-18 17:34:38 +0000

    Add image upload to firebase

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Frontend with github actions for forking
-Just switch 'frontend-fork' to the name of your app and you should be good to go!
+Image upload site for Kwame Dapaa
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Dropzone } from './styles';
+
+import ImageUpload from './ImageUpload';
+
 const App = () => {
-  return <p>Frontend fork by Cammy!</p>;
+  const [images, setImages] = useState<File[] | null>(null);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setImages(acceptedFiles);
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+    isDragActive,
+  } = useDropzone({ onDrop, accept: { 'image/*': [] } });
+
+  return (
+    <div>
+      <Dropzone {...getRootProps({ isFocused, isDragAccept, isDragReject })}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag and drop some files here, or click to select files</p>
+        )}
+      </Dropzone>
+      {images &&
+        images.map((image) => <ImageUpload key={image.name} image={image} />)}
+    </div>
+  );
 };
 
 export default App;
